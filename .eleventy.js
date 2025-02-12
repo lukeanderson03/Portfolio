16887b9bba5ff1e0040448330a474a6b339b29f6
Luke Anderson <ande2594@msu.edu>
2025-02-11 21:57:35 -0500

    Started homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("timeline", function (bulletColor, description) {
+    return `<section>
+      <div id="bullet-points" class="${bulletColor}"></div>
+      <p>${description}</p>
+    </section>`
+  });
 };
